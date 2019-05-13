4a1c459a0e8a1d90698305b3dc21e3d25f382fe6
mysitioc <mysitioc@mysitio.cl>
2019-05-13 13:48:34 -0500

    --- Auto-Git Commit ---

--- instabot/bot/bot_unfollow.py
+++ instabot/bot/bot_unfollow.py
@@ -18,11 +18,14 @@ def unfollow(self, user_id,progress=None):
         self.delay('unfollow')
         response=self.api.unfollow(user_id)
         if isinstance(response,bool) and response:
-            msg = '===> Unfollowed, `user_id`: {}, user_name: {}'
-            self.console_print(msg.format(user_id, username), 'yellow')
             self.unfollowed_file.append(user_id)
             self.total['unfollows'] += 1
             igtotal_model.set_total('unfollows',functions.current_time('%Y-%m-%d'),1)
+
+            msg = '===> Unfollowed, `user_id`: {}, user_name: {}. Total: {}/{}'
+            self.console_print(msg.format(user_id, username,self.total['unfollows'],self.self.max_per_day['unfollows']), 'yellow')
+
+
             if user_id in self.following:
                 self.following.remove(user_id)
             data={'id':user_info[0],'following':False}
