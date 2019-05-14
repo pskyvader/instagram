15c69d216d45b8d2df00ce2953519c43114b9d49
mysitioc <mysitioc@mysitio.cl>
2019-05-14 12:36:55 -0500

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -266,7 +266,7 @@ class app:
             post_env = app.environ.copy()
             post_env["QUERY_STRING"] = ""
             post_env["CONTENT_LENGTH"] = int(app.environ.get("CONTENT_LENGTH", 0))
-            print(post_env)
+            print(post_env["CONTENT_LENGTH"])
             p = FieldStorage(
                 fp=post_env["wsgi.input"], environ=post_env, keep_blank_values=True
             )
