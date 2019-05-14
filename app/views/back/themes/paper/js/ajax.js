476b51f959ce1fe375a9b2faa83bcecbcfd8165a
mysitioc <mysitioc@mysitio.cl>
2019-05-14 12:11:24 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/ajax.js
+++ app/views/back/themes/paper/js/ajax.js
@@ -51,7 +51,7 @@ function post(url_post, info, mensaje_inicial, importante, archivo, callback, ex
             data.append(key, value);
         });
         process=false;
-        type=false;
+        type='multipart/form-data';
     }else{
         process=true
         type='application/x-www-form-urlencoded; charset=UTF-8';
