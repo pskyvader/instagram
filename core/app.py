f1a459f24f37eb3f092bcbc4e478c5139c0757ed
mysitioc <mysitioc@mysitio.cl>
2019-05-14 13:38:14 -0500

    --- Auto-Git Commit ---

--- core/app.py
+++ core/app.py
@@ -268,6 +268,7 @@ class app:
             post_env["CONTENT_LENGTH"] = int(app.environ.get("CONTENT_LENGTH", 0))
 
             d = post_env['wsgi.input'].read(post_env["CONTENT_LENGTH"])
+            print(d)
             p = parse_qs(d)
             print(p)
 
