74073eadc48303848e07d1ddbd70749fc29b56dc
Pablo <pskyvader@hotmail.com>
2019-05-17 10:48:17 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/instagram.py
+++ app/controllers/back/themes/paper/instagram.py
@@ -208,7 +208,7 @@ class instagram(base):
             ig.bot.console_print("Hubo un error al dejar de seguir. Reiniciando bot para el siguiente paso")
             ig=instagram_bot()
 
-        #respuesta = ig.follow('hashtag')
+        respuesta = ig.follow('hashtag')
         ig.bot.console_print("Todos los pasos completados")
         if len(var)==0:
             ret["body"] = json.dumps(respuesta, ensure_ascii=False)
