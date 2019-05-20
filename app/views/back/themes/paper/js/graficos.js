a5de2d34e01e8cec015cb3ac3427705236524491
Pablo <pskyvader@hotmail.com>
2019-05-20 15:20:38 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -1,50 +1,47 @@
+var chart_backgrounds = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)'
+]
+var chart_borders = [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
+]
+
+
 function inicio_graficos() {
     chart_followers();
 }
 
-function chart_followers(){
-    var extra={
-        id:$('#chart-seguidores'),
-        type:'bar'
-    }
-
+function chart_followers() {
     post(create_url(modulo, accion), {
         id: id
-    }, mensaje, false, null, generar_grafico,extra);
-
-    var data={
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    }
-    generar_grafico(ctx,data,'bar');
+    }, mensaje, false, null, function(data){
+        
+        var data_response = {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: chart_backgrounds,
+                borderColor: chart_borders,
+                borderWidth: 1
+            }]
+        }
+        generar_grafico($('#chart-seguidores'), data_response, 'bar');
+    });
 }
 
 
 
-
-
-function generar_grafico(data,config) {
-    
+function generar_grafico(id,data,type) {
     var chart = new Chart(id, {
         type: type,
         data: data,
