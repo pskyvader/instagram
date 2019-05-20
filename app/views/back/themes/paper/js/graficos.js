578646f8f3f87937334b18b67d5611d8a9740ab5
Pablo <pskyvader@hotmail.com>
2019-05-20 16:12:55 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -21,29 +21,51 @@ function inicio_graficos() {
 }
 
 function chart_followers() {
-    var url=create_url(modulo, 'get_followers');
-    post_basic(url, {}, 'Adquiriendo usuarios',function(data){
-        var data_response=generar_response(data,'Usuarios');
-        generar_grafico($('#chart-seguidores'), data_response, 'bar');
+    var url = create_url(modulo, 'get_followers');
+    post_basic(url, {}, 'Adquiriendo usuarios', function(data) {
+        //var data_response = generar_response(data, 'Usuarios');
+        //generar_grafico($('#chart-seguidores'), data_response, 'bar');
+
+        var sets = [];
+        sets.push({
+            sets: ['Todos'],
+            size: data['Todos']
+        });
+        sets.push({
+            sets: ['Todos','Seguidores'],
+            size: data['Seguidores']
+        });
+        sets.push({
+            sets: ['Todos','Siguiendo'],
+            size: data['Siguiendo']
+        });
+        sets.push({
+            sets: ['Todos','Seguidores','Siguiendo'],
+            size: data['Seguidores y siguiendo']
+        });
+
+        var chart = venn.VennDiagram()
+        d3.select('#chart-seguidores').datum(sets).call(chart);
+
     });
 }
 
-function generar_response(initial_data,title){
-    var label=[]
-    var final_data=[]
-    var background=[]
-    var border=[]
-    var count_background=0;
-    var count_border=0;
-    $.each(initial_data,function(k,v){
+function generar_response(initial_data, title) {
+    var label = []
+    var final_data = []
+    var background = []
+    var border = []
+    var count_background = 0;
+    var count_border = 0;
+    $.each(initial_data, function(k, v) {
         label.push(k);
         final_data.push(v);
-        if(typeof(chart_backgrounds[count_background])=='undefined'){
-            count_background=0;
+        if (typeof(chart_backgrounds[count_background]) == 'undefined') {
+            count_background = 0;
         }
         background.push(chart_backgrounds[count_background]);
-        if(typeof(chart_borders[count_border])=='undefined'){
-            count_border=0;
+        if (typeof(chart_borders[count_border]) == 'undefined') {
+            count_border = 0;
         }
         border.push(chart_borders[count_border]);
         count_background++;
@@ -65,7 +87,7 @@ function generar_response(initial_data,title){
 
 
 
-function generar_grafico(id,data,type) {
+function generar_grafico(id, data, type) {
     var chart = new Chart(id, {
         type: type,
         data: data,
