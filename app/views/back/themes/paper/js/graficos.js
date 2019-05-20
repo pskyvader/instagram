71441a8e930311f3261cf8ad79560f686cc0a65d
Pablo <pskyvader@hotmail.com>
2019-05-20 18:11:15 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -31,57 +31,61 @@ function chart_followers() {
             sets: ['Seguidores'],
             label: 'Seguidores',
             size: data['follower'],
-            figure: data['follower'],
         });
         sets.push({
             sets: ['Siguiendo'],
             label: 'Siguiendo',
             size: data['following'],
-            figure: data['following'],
         });
         sets.push({
             sets: ['Seguidores', 'Siguiendo'],
             label: 'Seguidores y Siguiendo',
             size: data['both'],
-            figure: data['both'],
         });
-
-        var chart = venn.VennDiagram().width(500);
-        var div = d3.select("#chart-seguidores").datum(sets).call(chart);
-        div.selectAll(".venn-circle path").style("fill-opacity", .5).style("stroke-width", 3).style("stroke-opacity", 1);
-        div.selectAll("text").style("fill", "white");
-        var tooltip = d3.select("#chart-seguidores").append("div").attr("class", "venntooltip");
-        
-        var count=0;
-        $.each(div.selectAll("path")._groups[0],function(k,v){
-            $(v).css('fill',chart_borders[count]);
-            count++;
-        });
-
-
-        div.selectAll("g")
-            .on("mouseover", function(d, i) {
-                // sort all the areas relative to the current item
-                venn.sortAreas(div, d);
-
-                // Display a tooltip with the current size
-                tooltip.transition().duration(40).style("opacity", 1);
-                tooltip.text(d.size + " de los usuarios son " + d.label);
-                var selection = d3.select(this).transition("tooltip").duration(400);
-                var opacity=d.sets.length == 1 ? .8 : (d.sets.length > 1 ? .8 : 0);
-                selection.select("path").style("stroke-width", 3).style("fill-opacity", opacity).style("stroke", "fff");
-            })
-            .on("mousemove", function() {
-                tooltip.style("left", (d3.event.offsetX-30) + "px").style("top", (d3.event.offsetY + 50) + "px");
-            })
-            .on("mouseout", function(d, i) {
-                tooltip.transition().duration(100).style("opacity", 0);
-                var selection = d3.select(this).transition("tooltip").duration(200);
-                var opacity=d.sets.length == 1 ? .5 : 0;
-                selection.select("path").style("stroke-width", 3).style("fill-opacity", opacity).style("stroke", "none");
-            });
+        generar_venn(sets,"#chart-seguidores",'Usuarios');
     });
 }
+
+
+
+
+
+
+function generar_venn(sets,id,title) {
+    var chart = venn.VennDiagram().width(500);
+    var div = d3.select(id).datum(sets).call(chart);
+    div.selectAll(".venn-circle path").style("fill-opacity", .5).style("stroke-width", 3).style("stroke-opacity", 1);
+    div.selectAll("text").style("fill", "white");
+    var tooltip = d3.select(id).append("div").attr("class", "venntooltip");
+    var count = 0;
+    $.each(div.selectAll("path")._groups[0], function(k, v) {
+        $(v).css('fill', chart_borders[count]);
+        count++;
+    });
+
+
+    div.selectAll("g").on("mouseover", function(d, i) {
+        venn.sortAreas(div, d);
+        tooltip.transition().duration(40).style("opacity", 1);
+        tooltip.text(d.size + " "+title+" " + d.label);
+        var selection = d3.select(this).transition("tooltip").duration(400);
+        var opacity = d.sets.length == 1 ? .8 : (d.sets.length > 1 ? .8 : 0);
+        selection.select("path").style("stroke-width", 3).style("fill-opacity", opacity).style("stroke", "fff");
+    }).on("mousemove", function() {
+        tooltip.style("left", (d3.event.offsetX - 30) + "px").style("top", (d3.event.offsetY + 50) + "px");
+    }).on("mouseout", function(d, i) {
+        tooltip.transition().duration(100).style("opacity", 0);
+        var selection = d3.select(this).transition("tooltip").duration(200);
+        var opacity = d.sets.length == 1 ? .5 : 0;
+        selection.select("path").style("stroke-width", 3).style("fill-opacity", opacity).style("stroke", "none");
+    });
+
+
+}
+
+
+
+
 
 function generar_response(initial_data, title) {
     var label = []
