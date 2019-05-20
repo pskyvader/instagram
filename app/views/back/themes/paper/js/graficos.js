4af0e6306b3eae4fcf23cfe83cfb0aff5bd03a80
Pablo <pskyvader@hotmail.com>
2019-05-20 18:22:01 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -4,7 +4,8 @@ var chart_backgrounds = [
     'rgba(255, 206, 86, 0.2)',
     'rgba(75, 192, 192, 0.2)',
     'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)'
+    'rgba(255, 159, 64, 0.2)',
+    'rgb(44, 160, 44, 0.2)'
 ]
 var chart_borders = [
     'rgba(255, 99, 132, 1)',
@@ -12,13 +13,16 @@ var chart_borders = [
     'rgba(255, 206, 86, 1)',
     'rgba(75, 192, 192, 1)',
     'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)'
+    'rgba(255, 159, 64, 1)',
+    'rgb(44, 160, 44, 1)'
 ]
 
 
 function inicio_graficos() {
     chart_followers();
 }
+
+
 
 function chart_followers() {
     var url = create_url(modulo, 'get_followers');
