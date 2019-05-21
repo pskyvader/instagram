6774ccaf2ca09658d7a1603e4f8ea91c858c4bf7
Pablo <pskyvader@hotmail.com>
2019-05-20 20:14:04 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -60,7 +60,7 @@ function generar_venn(sets, id, title) {
     div.selectAll("text").style("fill", "white");
     var tooltip = d3.select(id).append("div").attr("class", "venntooltip");
     $.each(div.selectAll("path")._groups[0], function(k, v) {
-        $(v).css('fill', randomColor({luminosity: 'bright'}));
+        $(v).css('fill', randomColor({luminosity: 'dark'}));
     });
     div.selectAll("g").on("mouseover", function(d, i) {
         venn.sortAreas(div, d);
