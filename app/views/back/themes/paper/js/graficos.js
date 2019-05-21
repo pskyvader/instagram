6892e6ab3a1c9a1e27fa40bcdbb3352696842563
Pablo <pskyvader@hotmail.com>
2019-05-21 11:01:51 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -186,7 +186,7 @@ function generar_response(initial_data, title, hue) {
 }
 
 function generar_grafico(id, data, type, options) {
-    console.log($(id).siblings().find('progress'));
+    console.log($($(id).siblings()).find('progress'));
     if (typeof(options) == 'undefined') {
         var options = {
             scales: {
