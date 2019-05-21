3628aee4efd99ab8b91bf8f4a92960b7f14c0434
Pablo <pskyvader@hotmail.com>
2019-05-21 11:40:22 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/home.py
+++ app/controllers/back/themes/paper/home.py
@@ -98,7 +98,7 @@ class home(base):
             f = igaccounts_model.getAll( {"hashtag": h["hashtag"],'follower':True}, select="total" )
             fl = igaccounts_model.getAll( {"hashtag": h["hashtag"],'following':True}, select="total" )
             r = igaccounts_model.getAll( {"hashtag": h["hashtag"],'following':False}, select="total" )
-            porcentaje=f/(fl+r)*100;
+            porcentaje=(f/(fl+r))*100;
             nombre=+' ('+str(porcentaje)+'%)'
             followers[nombre]=f
             following[nombre]=fl
