34acb07fa27734db40500f33f4272173bfd07f21
Pablo <pskyvader@hotmail.com>
2019-05-23 13:34:38 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/instagram_bot.py
+++ app/controllers/back/themes/paper/instagram_bot.py
@@ -193,8 +193,8 @@ class instagram_bot():
                         if not bot.reached_limit('follows'):
                             bot.console_print("Siguiendo usuarios con hashtag: " + h)
                             users = bot.get_hashtag_users(h)
-                            #primeros 50 usuarios solamente, para emparejar la cantidad de usuarios por hashtag
-                            users=users[:50]
+                            #primeros 20 usuarios solamente, para emparejar la cantidad de usuarios por hashtag
+                            users=users[:20]
                             bot.follow_users(users,base,proporcion,h)
                             if bot.api.fatal_error:
                                 respuesta['exito']=False
