ad8b851c0795354e5a92e31b89e0a1eb6cfab2b3
Pablo <pskyvader@hotmail.com>
2019-05-31 14:28:35 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -20,7 +20,7 @@ function inicio_graficos() {
 function chart_total() {
     var url = create_url(modulo, 'get_total_followers');
     post_basic(url, {}, 'Adquiriendo Seguidores Totales', function(data) {
-        var data_follower = generar_response(data.follower, 'Seguidores', 'yellow');
+        var data_follower = generar_response(data.follower, 'Seguidores', 'purple');
         var data_following = generar_response(data.following, 'Siguiendo', 'red');
         data_follower.datasets = [
             data_follower.datasets[0],
