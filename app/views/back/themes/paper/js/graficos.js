acac2e76d4577c8afc93e237e5d01cb2d484608d
Pablo <pskyvader@hotmail.com>
2019-05-31 15:02:51 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -230,7 +230,8 @@ function generar_venn(sets, id, title) {
     $(progress).slideUp();
     $.each(div.selectAll("path")._groups[0], function(k, v) {
         $(v).css('fill', randomColor({
-            luminosity: 'dark'
+            luminosity: 'dark',
+            hue: 'random'
         }));
     });
     div.selectAll("g").on("mouseover", function(d, i) {
