4151dab8b41729f8bb8bd72c3c5455d30b7b2abf
Pablo <pskyvader@hotmail.com>
2019-06-02 14:09:54 -0500

    --- Auto-Git Commit ---

--- instabot/bot/bot_follow.py
+++ instabot/bot/bot_follow.py
@@ -28,7 +28,7 @@ def follow(self, user_id,force=False,hashtag='',progress=None):
             self.console_print(msg, 'green')
             
             if user_id not in self.following:
-                self.following.append(user_id)
+                self.following.append(user_id,show_message=False)
             
             if not error_check:
                 data={'id':user_info[0],'following':True}
