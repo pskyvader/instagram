f34373655004d27b2c30d4429296b64faa634f20
Pablo <pskyvader@hotmail.com>
2019-06-03 14:38:30 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/functions.js
+++ app/views/back/themes/paper/js/functions.js
@@ -384,10 +384,12 @@ $.fn.serializeObject = function() {
 
 $.fn.equals = function(compareTo) {
     if (!compareTo || this.length != compareTo.length) {
+        console.log(compareTo,this.length,compareTo.length);
         return false;
     }
     for (var i = 0; i < this.length; ++i) {
         if (this[i] !== compareTo[i]) {
+            console.log(this[i],compareTo[i]);
             return false;
         }
     }
