d3e0b70ec825285f073e81cd006b5cde1786a08d
Pablo <pskyvader@hotmail.com>
2019-06-03 14:17:59 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/graficos.js
+++ app/views/back/themes/paper/js/graficos.js
@@ -190,7 +190,7 @@ function chart_followers() {
         ];
 
 
-        generar_venn(sets, "#chart-seguidores", 'Usuarios');
+        generar_venn(sets, "#chart-seguidores", 'Usuarios', 'black');
         $(window).on('resize', function() {
             if ($("#chart-seguidores").length > 0) {
                 var width = $("#chart-seguidores").innerWidth();
