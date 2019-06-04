6b8083065c51f56c44c42ef2b97b3af3e17a259a
Pablo <pskyvader@hotmail.com>
2019-06-04 11:56:04 -0500

    --- Auto-Git Commit ---

--- app/views/back/themes/paper/js/functions.js
+++ app/views/back/themes/paper/js/functions.js
@@ -383,23 +383,27 @@ $.fn.serializeObject = function() {
 };
 
 const isEqual = (a, b) => {
+    var log=true;
     if (a === b) return true;
-    if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime();
+    if (a instanceof Date && b instanceof Date) {
+        if (log) console.log(a.getTime(),b.getTime(),'time');
+        return a.getTime() === b.getTime();
+    }
     if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')){
-        console.log(a,b,a===b);
+        if (log) console.log(a,b,a===b);
         return a === b;
     } 
     if (a === null || a === undefined || b === null || b === undefined){
-        console.log(a,b,'null');
+        if (log) console.log(a,b,'null');
         return false;
     }
     if (a.prototype !== b.prototype){
-        console.log(a.prototype,b.prototype,'prototype');
+        if (log) console.log(a.prototype,b.prototype,'prototype');
         return false;
     } 
     let keys = Object.keys(a);
     if (keys.length !== Object.keys(b).length){
-        console.log(a,b,'keys');
+        if (log) console.log(a,b,'keys');
         return false;
     } 
     return keys.every(k => isEqual(a[k], b[k]));
