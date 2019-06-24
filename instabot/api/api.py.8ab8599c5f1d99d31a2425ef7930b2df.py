9757acc72b931f0192292511642d45485d454a90
Pablo <pskyvader@hotmail.com>
2019-06-24 14:36:25 -0500

    --- Auto-Git Commit ---

--- instabot/api/api.py.8ab8599c5f1d99d31a2425ef7930b2df.py
+++ instabot/api/api.py.8ab8599c5f1d99d31a2425ef7930b2df.py
@@ -242,14 +242,7 @@ class API(object):
                 response_data = {"message": response.text}
 
             bot_support.console_print(
-                bot_support,
-                "Error detail:"
-                + repr(response)
-                + ".\nEndpoint:"
-                + endpoint
-                + ".\npost:"
-                + repr(post_tmp),
-                "red",
+                bot_support, "Error detail:" + repr(response) + ".\nEndpoint:" + endpoint + ".\npost:" + repr(post_tmp), "red"
             )
 
             if "feedback_required" in str(response_data.get("message")):
