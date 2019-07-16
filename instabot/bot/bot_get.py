3c293db3158d789ea5d29e9e1faf043a881ccd58
Pablo <pskyvader@hotmail.com>
2019-07-16 13:32:04 -0500

    --- Auto-Git Commit ---

--- instabot/bot/bot_get.py
+++ instabot/bot/bot_get.py
@@ -30,8 +30,7 @@ def get_popular_medias(self):
 def get_tags(self, query):
     print('before', query)
     self.api.search_tags(query)
-    print('after')
-    print(self.api.last_json)
+    print('after',self.api.last_json)
     return [str(media["name"]) for media in self.api.last_json["results"]]
 
 
