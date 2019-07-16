822220998169b3ddeda4f765e7c9d0cbad418c9e
Pablo <pskyvader@hotmail.com>
2019-07-16 14:35:44 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/instagram_bot.py
+++ app/controllers/back/themes/paper/instagram_bot.py
@@ -418,7 +418,7 @@ class instagram_bot:
         if len(hashtag_list["total"]) >= 15:
             # desactivar el peor hashtag. conservar para evitar agregarlo nuevamente
             # si hay 15 elementos, se comparan los primeros 10
-            print(hashtag_list["total"],[hashtag_list["total"].values()])
+            print(hashtag_list["total"],list(hashtag_list["total"].values()),list(hashtag_list["total"].values())[9])
             menor_list = [hashtag_list["total"].values()][9]
             if menor_list > 1000:
                 hashtag_eficiencia= {k: hashtag_list["eficiencia2"][k] for k in list(hashtag_list["eficiencia2"])[:10]}
