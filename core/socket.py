fdb1c95661af40e7cfdaf6d530cff75e05f20e0a
Pablo <pskyvader@hotmail.com>
2019-07-26 18:34:54 -0500

    --- Auto-Git Commit ---

--- core/socket.py
+++ core/socket.py
@@ -5,52 +5,66 @@ import json
 class socket:
     sock = None
     host = "ws://socket.mysitio.cl:8000/ws"
-    url="http://socket.mysitio.cl/"
-    intento=False
+    url = "http://socket.mysitio.cl/"
+    intento = False
 
     @staticmethod
     def send(msg):
-        try:
-            if socket.sock==None and not socket.intento:
-                down=socket.download(socket.url+"port.txt")
-                socket.host=json.loads(down)['final_url']
-                socket.sock = create_connection(socket.host)
-            if socket.sock!=None:
-                socket.sock.send(msg)
-        except Exception as e:
-            print("error:",e)
-            socket.sock=None
-            if not socket.intento:
-                socket.intento=True
-                socket.create()
+        socket.create()
+        if socket.sock != None:
+            socket.sock.send(msg)
 
+    # @staticmethod
+    # def send(msg):
+    #     try:
+    #         if socket.sock==None and not socket.intento:
+    #             down=socket.download(socket.url+"port.txt")
+    #             socket.host=json.loads(down)['final_url']
+    #             socket.sock = create_connection(socket.host)
+    #         if socket.sock!=None:
+    #             socket.sock.send(msg)
+    #     except Exception as e:
+    #         print("error:",e)
+    #         socket.sock=None
+    #         if not socket.intento:
+    #             socket.intento=True
+    #             socket.create()
 
     @staticmethod
     def create():
         import urllib.request
-        url=socket.url
+
+        if socket.sock != None:
+            return
+
+        url = socket.url
         try:
-            response = urllib.request.urlopen(url,timeout=1)
-            if socket.sock==None:
-                down=socket.download(socket.url+"port.txt")
-                socket.host=json.loads(down)['final_url']
-                socket.sock = create_connection(socket.host)
-        except:
-            socket.sock=None
+            if socket.intento:
+                response = urllib.request.urlopen(url, timeout=1)
+            down = socket.download(socket.url + "port.txt")
+            socket.host = json.loads(down)["final_url"]
+            socket.sock = create_connection(socket.host)
+        except Exception as e:
+            print("error:", e)
+            socket.sock = None
+            if not socket.intento:
+                socket.intento = True
+                socket.create()
 
     @staticmethod
     def close():
-        if socket.sock!=None:
+        if socket.sock != None:
             socket.sock.close()
-            socket.sock=None
+            socket.sock = None
 
     @staticmethod
     def download(url):
         import urllib.request
+
         try:
-            response =  urllib.request.urlopen(url)
-            return response.read().decode('utf-8')
+            response = urllib.request.urlopen(url)
+            return response.read().decode("utf-8")
         except:
-            return 'Error al obtener el archivo '+url+'. Intenta mas tarde'
+            return "Error al obtener el archivo " + url + ". Intenta mas tarde"
         return True
 
