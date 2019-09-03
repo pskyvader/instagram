8b2de0daa99ffd38e529b213e2d0c6bc28bdb030
Pablo <pskyvader@hotmail.com>
2019-09-03 14:29:31 -0500

    --- Auto-Git Commit ---

--- instabot/bot/bot_archive.py
+++ instabot/bot/bot_archive.py
@@ -6,38 +6,40 @@ def archive(self, media_id, undo=False):
     media = self.get_media_info(media_id)
     media = media[0] if isinstance(media, list) else media
     if self.api.archive_media(media, undo):
-        self.total['archived'] += int(not undo)
-        self.total['unarchived'] += int(undo)
+        self.total["archived"] += int(not undo)
+        self.total["unarchived"] += int(undo)
         return True
-    self.console_print("Media id %s is not %s.", media_id, 'unarchived' if undo else 'archived')
+    self.logger.info(
+        "Media id %s is not %s.", media_id, "unarchived" if undo else "archived"
+    )
     return False
 
 
 def archive_medias(self, medias):
     broken_items = []
     if not medias:
-        self.console_print("Nothing to archive.")
+        self.logger.info("Nothing to archive.")
         return broken_items
-    self.console_print("Going to archive %d medias." % (len(medias)))
+    self.logger.info("Going to archive %d medias." % (len(medias)))
     for media in tqdm(medias):
         if not self.archive(media):
             self.error_delay()
-            broken_items = medias[medias.index(media):]
+            broken_items = medias[medias.index(media) :]
             break
-    self.console_print("DONE: Total archived %d medias." % self.total['archived'])
+    self.logger.info("DONE: Total archived %d medias." % self.total["archived"])
     return broken_items
 
 
 def unarchive_medias(self, medias):
     broken_items = []
     if not medias:
-        self.console_print("Nothing to unarchive.")
+        self.logger.info("Nothing to unarchive.")
         return broken_items
-    self.console_print("Going to unarchive %d medias." % (len(medias)))
+    self.logger.info("Going to unarchive %d medias." % (len(medias)))
     for media in tqdm(medias):
         if not self.unarchive(media):
             self.error_delay()
-            broken_items = medias[medias.index(media):]
+            broken_items = medias[medias.index(media) :]
             break
-    self.console_print("DONE: Total unarchived %d medias." % self.total['unarchived'])
+    self.logger.info("DONE: Total unarchived %d medias." % self.total["unarchived"])
     return broken_items
