5643c48df061457bae6969e346b4c3d3acf6a2b3
Pablo <pskyvader@hotmail.com>
2019-09-03 14:29:46 -0500

    --- Auto-Git Commit ---

--- instabot/bot/bot_block.py
+++ instabot/bot/bot_block.py
@@ -7,59 +7,61 @@ def block(self, user_id):
     user_id = self.convert_to_user_id(user_id)
     if self.check_not_bot(user_id):
         return True
-    if not self.reached_limit('blocks'):
-        self.delay('block')
+    if not self.reached_limit("blocks"):
+        self.delay("block")
         if self.api.block(user_id):
-            self.total['blocks'] += 1
+            self.total["blocks"] += 1
             return True
     else:
-        self.console_print("Out of blocks for today.")
+        self.logger.info("Out of blocks for today.")
     return False
 
 
 def unblock(self, user_id):
     user_id = self.convert_to_user_id(user_id)
-    if not self.reached_limit('unblocks'):
-        self.delay('unblock')
+    if not self.reached_limit("unblocks"):
+        self.delay("unblock")
         if self.api.unblock(user_id):
-            self.total['unblocks'] += 1
+            self.total["unblocks"] += 1
             return True
     else:
-        self.console_print("Out of blocks for today.")
+        self.logger.info("Out of blocks for today.")
     return False
 
 
 def block_users(self, user_ids):
     broken_items = []
-    self.console_print("Going to block %d users." % len(user_ids))
+    self.logger.info("Going to block %d users." % len(user_ids))
     for user_id in tqdm(user_ids):
         if not self.block(user_id):
             self.error_delay()
-            broken_items = user_ids[user_ids.index(user_id):]
+            broken_items = user_ids[user_ids.index(user_id) :]
             break
-    self.console_print("DONE: Total blocked %d users." % self.total['blocks'])
+    self.logger.info("DONE: Total blocked %d users." % self.total["blocks"])
     return broken_items
 
 
 def unblock_users(self, user_ids):
     broken_items = []
-    self.console_print("Going to unblock %d users." % len(user_ids))
+    self.logger.info("Going to unblock %d users." % len(user_ids))
     for user_id in tqdm(user_ids):
         if not self.unblock(user_id):
             self.error_delay()
             broken_items.append(user_id)
-    self.console_print("DONE: Total unblocked %d users." % self.total['unblocks'])
+    self.logger.info("DONE: Total unblocked %d users." % self.total["unblocks"])
     return broken_items
 
 
 def block_bots(self):
-    self.console_print("Going to block bots.")
+    self.logger.info("Going to block bots.")
     your_followers = self.followers
     your_likers = self.get_user_likers(self.user_id)
     not_likers = list(set(your_followers) - set(your_likers))
     random.shuffle(not_likers)
     for user in tqdm(not_likers):
         if not self.check_not_bot(user):
-            self.console_print("Found bot: "
-                             "https://instagram.com/%s/" % self.get_user_info(user)["username"])
+            self.logger.info(
+                "Found bot: "
+                "https://instagram.com/%s/" % self.get_user_info(user)["username"]
+            )
             self.block(user)
