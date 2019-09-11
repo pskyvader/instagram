7bf1be058b657926cad35365f305898bc3fd18d7
Pablo <pskyvader@hotmail.com>
2019-09-10 19:07:50 -0500

    --- Auto-Git Commit ---

--- app/controllers/back/themes/paper/instagram.py
+++ app/controllers/back/themes/paper/instagram.py
@@ -220,13 +220,13 @@ class instagram(base):
             configuracion_model.setByVariable(
                 "daily_process_hours", json.dumps(daily_process_hours)
             )
-            cookie_name = configuracion_model.getByVariable(
-                "cookie_name", "cookie_usuario"
-            )
-            if ".json" not in cookie_name:
-                cookie_name += ".json"
-            if os.path.exists(cookie_name):
-                os.remove(cookie_name)
+            # cookie_name = configuracion_model.getByVariable(
+            #     "cookie_name", "cookie_usuario"
+            # )
+            # if ".json" not in cookie_name:
+            #     cookie_name += ".json"
+            # if os.path.exists(cookie_name):
+            #     os.remove(cookie_name)
         if hora not in daily_process_hours:
             respuesta["mensaje"] = "Fuera de horario activo: " + hora
             if len(var) == 0:
